9230fd8c1684c0cfdb20c34c4644ff11e553b750
wzj <jay_wzj@163.com>
2018-05-29 11:36:44 +0800

    1

---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="i_warp">
+    <div class="i_warp_banner"></div>
+    <div class="i_warp_zmd">
+      <div class="name"><i></i><span>最新消息</span></div>
+      <div class="marquee">
+        <div>金卡会员刘**刚刚兑换了一张iPhoneX 800元代金券</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import '@/assets/index.css';
+
+  export default {
+    name: 'index',
+    data() {
+      return {}
+    }
+  }
+</script>
